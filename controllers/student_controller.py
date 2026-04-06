20764137f7d5863d5595fc46abd277db8f644498
devashreebhole0506 <devashreebhole562@gmail.com>
2026-04-06 20:20:53 +0530

    add my first code

---
+++ controllers/student_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify, render_template
+from services.student_service import StudentService
+
+student_bp = Blueprint('student', __name__)
+
+@student_bp.route('/')
+def form():
+    return render_template('form.html')
+
+
+@student_bp.route('/api/students', methods=['POST'])
+def create_student():
+
+    data = {
+        "name": request.form['name'],
+        "address": request.form['address'],
+        "mobile": request.form['mobile'],
+        "email": request.form['email'],
+        "education": request.form['education'],
+        "gender": request.form['gender']
+    }
+
+    result = StudentService.register_student(data)
+
+    return jsonify(result)
